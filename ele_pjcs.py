003b84e420b2264d1b3a1a3b96105a45ad0b7296
Qcby <35736643+Qcby@users.noreply.github.com>
2023-07-31 10:35:44 +0800

    联通性检测

--- ele_pjcs.py
+++ ele_pjcs.py
@@ -19,3 +19,14 @@ try:
 except requests.exceptions.RequestException as e:
     print('请求错误:', response.text,e)
 
+url = 'http://fn.syyx.com/my_ip'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+try:
+    response = requests.get(url, headers=headers)
+    print('调试成功: 你的IP是 『',response.text,'』')
+
+except requests.exceptions.RequestException as e:
+    print('请求错误:', response.text,e)
